44a5e6ab0251d5ebca779da30ea821c453611630
Jack Roche <52017135+jackr271@users.noreply.github.com>
2024-02-20 17:35:12 -0500

    feat: finish etch-a-sketch js funcitonality

--- javascript.js
+++ javascript.js
@@ -3,6 +3,19 @@ buildGrid(squareDimension);
 
 const resizeButton = document.querySelector('#resize');
 const clearButton = document.querySelector('#clear');
+const boxGrid = document.querySelector('.box-grid');
+
+let mouseDown = false;
+
+/*boxGrid.addEventListener = ('mousedown', ()=> {
+    console.log('down');
+    mouseDown = true;
+});
+boxGrid.addEventListener = ('mouseup', ()=> {
+    console.log('up');
+    mouseDown = false;
+});*/
+
 
 resizeButton.addEventListener('click', ()=> {
     let number = -1;
@@ -12,7 +25,31 @@ resizeButton.addEventListener('click', ()=> {
     buildGrid(number);
 });
 
-function clearGrid (grid) {
+clearButton.addEventListener('click', ()=> {
+    squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+    });
+})
+
+document.addEventListener('mousedown', () => {
+    mouseDown = true;
+});
+
+document.addEventListener('mouseup', () => {
+    mouseDown = false;
+});
+
+boxGrid.addEventListener('mouseover', (Event) => {
+    if (mouseDown) {
+        let target = Event.target;
+        if (target.classList.contains('square')) {
+        darkenSquare(target);
+        }
+    }
+});
+
+function emptyGrid (grid) {
     while (grid.firstChild) {
         grid.removeChild(grid.firstChild);
     }
@@ -21,14 +58,12 @@ function clearGrid (grid) {
 function buildGrid (sideLength) {
     const grid = document.querySelector('.box-grid');
 
-    clearGrid(grid);
+    emptyGrid(grid);
 
     for (let i = 0; i < sideLength; i++) {
         const column = buildColumn(sideLength);
         grid.appendChild(column);
     }
-
-    return grid;
 }
 
 function buildColumn (length) {
@@ -38,9 +73,22 @@ function buildColumn (length) {
     for (let j = 0; j < length; j++) {
         const square = document.createElement('div');
         square.className = 'square';
+
         column.appendChild(square);
     }
 
     return column;
 }
 
+function darkenSquare (square) {
+    const currentColor = getComputedStyle(square).getPropertyValue('background-color');
+    if (currentColor.substring(0, 4) != 'rgba') {
+        return;
+    } 
+    else if (currentColor.at(-2) === '0') {
+        square.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+    }
+    else {
+        square.style.backgroundColor = `rgba(0, 0, 0, ${+currentColor.at(-2)/10 + 0.1})`;
+    }
+}
